dce7d0e42822e5cb8a94b77ca773130cb041e0a8
bedena2000 <bedena2000@gmail.com>
2022-12-11 18:24:59 +0400

    first commit

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MainPage.scss';
+import AbcIcon from '@mui/icons-material/Abc';
+
+const MainPage = () => {
+  return (
+    <div className='main-page'>
+      
+    </div>
+  )
+}
+
+export default MainPage;
