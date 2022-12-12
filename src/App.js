93b2a59f91803f1ba0f170c748cb73d2a51747d1
bedena2000 <bedena2000@gmail.com>
2022-12-12 07:43:41 +0400

    added background on menu

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 // Components
-import { Menu } from "./components/Menu/Menu";
-import { MenuBurger } from "./components/MenuBurger/MenuBurger";
+import { Menu } from './components/Menu/Menu';
+import { MenuBurger } from './components/MenuBurger/MenuBurger';
+import { Background } from './components/background/Background';
 // Pages
-import MainPage from "./pages/MainPage/MainPage";
+import MainPage from './pages/MainPage/MainPage';
 
 function App() {
-  return <div className="app">
-    <MainPage />
-    <Menu />
-    <MenuBurger />
-  </div>;
+  return (
+    <div className="app">
+      <MainPage />
+      <Menu />
+      <MenuBurger />
+      <Background />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/background/Background.jsx
@@ -0,0 +1,17 @@
+import './Background.scss';
+import { useSelector, useDispatch } from 'react-redux';
+import { burgerActiveSelector } from '../../app/selectors/selectors';
+import { changeBurgerActive } from '../../app/slices/menuSlice';
+
+export const Background = () => {
+  const dispatch = useDispatch();
+  const burgerState = useSelector(burgerActiveSelector);
+  console.log(burgerState);
+
+  const onBackgroundClick = () => {
+    const action = changeBurgerActive();
+    dispatch(action);
+  };
+
+  return <div className={`background ${burgerState ? 'background-active' : 'background-not-active'}`} onClick={onBackgroundClick}></div>;
+};
